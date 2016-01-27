2cc136ad5f15961190a5e6d67aaf7be99fb3987a
TalentMicro <s.hariharan90@gmail.com>
2016-01-27 20:09:28 +0530

    Initial commit

---
+++ hariRepoPractice/hariRepoPractice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  hariRepoPractice
+//
+//  Created by TalentMicro on 27/01/16.
+//  Copyright © 2016 TalentMicro Innovation Pvt Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ hariRepoPractice/hariRepoPractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hariRepoPractice
+//
+//  Created by TalentMicro on 27/01/16.
+//  Copyright © 2016 TalentMicro Innovation Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
